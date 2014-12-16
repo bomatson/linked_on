811729c03ba373ff18675abb2c20f2c1e49d8085
bomatson <bobby@carbonfive.com>
2014-12-15 20:31:20 -0800

    use carrierwave

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :organization_id)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :organization_id, :avatar)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209053926) do
+ActiveRecord::Schema.define(version: 20141216035642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20141209053926) do
     t.datetime "updated_at",      null: false
     t.integer  "organization_id"
     t.string   "password_digest"
+    t.string   "avatar"
   end
 
 end
